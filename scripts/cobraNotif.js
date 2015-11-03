24bd2cfde67af3372a0e6d117d2bbf47a6f9dbb9
Yahia <yroyro59@gmail.com>
2015-11-03 19:34:49 +0100

    Ajout du traitement de la partie connexion

--- scripts/cobraNotif.js
+++ scripts/cobraNotif.js
@@ -1,4 +1,38 @@
 var cobra = new Cobra();
-        var room = "notifications6-yahia";
-        var roomPseudo = 'BDDPseudo4'
+
+var room = "notifications6-yahia";
+var roomPseudo = 'BDDPseudo4'
+var socketId;
+var apiUrl = 'http://cobra-framework.com:3000/api/events/' + room;
+cobra.connect('http://cobra-framework.com:8080');
+
+cobra.connectionCallback = function () {
+	cobra.joinRoom(room);
+}
+
+       cobra.messageReceivedCallback = function (message) {
+            // Lors de l'arrivée dans une room donne la liste des utilisateurs contenus dans la room
+            if(message.type == "infos"){
+            	for(var i = 0; i < message.clients.length; i++)
+            	{
+                    // Contient l'id du client
+                    var client = message.clients[i];
+                    
+                }
+                // Mon id attribué par la room
+                socketId = message.socketId;
+            }
+            else if (message.message) {
+               // Message reçu, je le traite
+               console.log(message.message.content);
+           }
+       }
        
+       cobra.clientJoinedRoomCallback = function(data){
+            // Un autre client a rejoint la room
+        }
+        
+        cobra.clientLeftRoomCallback = function(data){
+            // Un client a quitté la room
+        }
+
--- scripts/mainController.js
+++ scripts/mainController.js
@@ -1,13 +1,58 @@
 var app = angular.module('notifApp',[]);
+
+app.run(function($rootScope) {
+  $rootScope.divAccueil = false;
+  $rootScope.divNotifications = false;
+  $rootScope.divInfo = false;
+  $rootScope.divMenu = false;
+  $rootScope.divConnexion = true;
+});
+
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
   $http.get('http://cobra-framework.com:3000/api/events/notifications6-yahia').success(function(data) {
-        $scope.messages = [];
-        console.log(data.Events.length);
-        for (var i = 0; i < data.Events.length; i++) {
-        	var contenuMsg = (JSON.parse(data.Events[i].content));
-        	$scope.messages.push(contenuMsg);
+    $scope.messages = [];
+    console.log(data.Events.length);
+    for (var i = 0; i < data.Events.length; i++) {
+     var contenuMsg = (JSON.parse(data.Events[i].content));
+     $scope.messages.push(contenuMsg);
+   }
 
-        }
-
-    });
+ });
 }]);
+
+
+app.controller('ConnexionController', ['$scope', '$rootScope','$http', function($scope, $rootScope, $http) {
+
+  $scope.update = function(user) {
+        //On recupere la valeur des champ et on les enregistre dans le scoop
+        $scope.pseudo = angular.copy(user.pseudo);
+        $scope.mdp = angular.copy(user.mdp);
+        //On traite la connexion
+        $http.get('http://cobra-framework.com:3000/api/events/BDDPseudo4').success(function(data) {
+          $scope.messages = [];
+          var mdp;
+          var pseudo;
+          for (var i = 0; i < data.Events.length; i++) {
+            var contenuBDD = (JSON.parse(data.Events[i].content));
+            mdp = contenuBDD.message.mdp;
+            pseudo = contenuBDD.message.pseudo;
+            if($scope.pseudo == pseudo && $scope.mdp == mdp){
+              $rootScope.divAccueil = true;
+              $rootScope.divNotifications = true;
+              $rootScope.divInfo = true;
+              $rootScope.divMenu = true;
+              $rootScope.divConnexion = false;
+              return true;
+            }
+          }
+          alert("pseudo ou mot de passe non valide");
+        });
+        
+      };
+
+      $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+      };
+
+      $scope.reset();
+    }]);
